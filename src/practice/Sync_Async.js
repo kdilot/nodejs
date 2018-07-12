f9abdf34139013c00330f2c0eb63b40f09d3d9c7
kdilot <kdilot@gmail.com>
2018-07-11 18:14:43 -0700

    sYNCHronous & Asynchronous

---
+++ src/practice/Sync_Async.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+
+// Sync
+console.log(1);
+var data = fs.readFileSync('SyncData.txt', {encoding:'utf8'});
+console.log(data);
+
+// Async
+console.log(2);
+fs.readFile('SyncData.txt', {encoding:'utf8'}, function(err, data) {
+    console.log(3);
+    console.log(data);
+});
+
+console.log(4);
+
+// console 1 > 2 > 4 > 3
